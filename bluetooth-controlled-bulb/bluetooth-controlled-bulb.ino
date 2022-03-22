7aa1596036d15befcc3c3e679c5a489a576cb217
Von <vonshirco.vs@gmail.com>
2022-03-22 20:06:01 +0300

    Update bluetooth-controlled-bulb.ino

--- bluetooth-controlled-bulb/bluetooth-controlled-bulb.ino
+++ bluetooth-controlled-bulb/bluetooth-controlled-bulb.ino
@@ -1,4 +1,5 @@
 //Project Descriptions
+
 /* Connections: 
 Bluetooth module TX to D10
 Bluetooth module RX to D11
@@ -8,12 +9,18 @@ Bluetooth module GND to GND
 
 #include <SoftwareSerial.h>
 
+softwareSerial BT(10, 11);
 
+#define SW 2 //define pin 2 as control pin
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(SW, OUTPUT); 
+  BT.begin(9600); //Data rate for the softwareSerial port
+  BT.println("Hello from Arduino"); //Send test message to other device
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
