3630f0a2ab641ca577d49c7fa1fedfc09c04f2d5
Von <vonshirco.vs@gmail.com>
2022-03-22 19:55:17 +0300

    Update bluetooth-controlled-bulb.ino

--- bluetooth-controlled-bulb/bluetooth-controlled-bulb.ino
+++ bluetooth-controlled-bulb/bluetooth-controlled-bulb.ino
@@ -1,3 +1,15 @@
+//Project Descriptions
+/* Connections: 
+Bluetooth module TX to D10
+Bluetooth module RX to D11
+Bluetooth module Vcc to 5V
+Bluetooth module GND to GND
+*/
+
+#include <SoftwareSerial.h>
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
